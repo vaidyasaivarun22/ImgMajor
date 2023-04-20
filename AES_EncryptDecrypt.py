f73eccbc25c651c41a8db7eb03c4b99dae8482fe
VARUN_PANGEAN <saivarun.v@pangean.global.com>
2023-04-21 01:16:06 +0530

    -added major changes in GUI and logic

--- AES_EncryptDecrypt.py
+++ AES_EncryptDecrypt.py
@@ -5,37 +5,66 @@ from PIL import Image
 import math
 from random import randrange, seed
 
-def aes_encrypt(u,key,f):
+def aes_encrypt(u,key,f,msg=None):
     fin = open(f,'rb') 
     image = fin.read() 
     fin.close() 
-    key=hashlib.sha512((str(key)).encode("utf-8")).digest()
+    salt =os.urandom(16)
+    image =salt+image
+    key=hashlib.sha256((str(key)).encode("utf-8")).digest()
     cipher = AES.new(key, AES.MODE_EAX)
     nonce = cipher.nonce
     ciphertext,tag= cipher.encrypt_and_digest(image)
-
+    #stegnography
+    if msg:
+        steg_image = Image.frombytes("RGB", (int(math.sqrt(len(ciphertext))), int(math.sqrt(len(ciphertext)))), ciphertext)
+        steg_image.putpixel((0, 0), (len(msg), len(msg)))
+        i, j = 1, 0
+        for c in msg:
+            steg_image.putpixel((i, j), (ord(c), 0, 0))
+            i += 1
+            if i >= steg_image.width:
+                i = 1
+                j += 1
+        steg_image_bytes = steg_image.tobytes()
+        ciphertext = steg_image_bytes
     path=os.path.join(u,"encrypt")
     file1=open(os.path.join(path,"aesout.jpg"),"wb")
     file2=open(os.path.join(path,"n.txt"),"wb")
+    file3=open(os.path.join(path,"s.txt"),"wb")
     file2.write(nonce)
+    file3.write(salt)
     file1.write(ciphertext)
     file1.close()
     file2.close()
+    file3.close()
 
 def aes_decrypt(u,key,f):
-    key=hashlib.sha512((str(key)).encode("utf-8")).digest()
+    key=hashlib.sha256((str(key)).encode("utf-8")).digest()
     path=os.path.join(u,"encrypt")
 
     file1 = open(f,'rb') 
-    file3=open(os.path.join(path,"n.txt"),"rb")
+    file2=open(os.path.join(path,"n.txt"),"rb")
+    file3=open(os.path.join(path,"s.txt"),"rb")
     image = file1.read() 
-    nonce=file3.read()
+    nonce=file2.read()
+    salt =file3.read()
     file1.close() 
+    file2.close()
     file3.close()
 
     cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
     plaintext = cipher.decrypt(image)
+    # Verify that the decrypted image starts with the expected salt value
+    if plaintext[:16] != salt:
+        raise ValueError("Incorrect key or corrupted data")
 
+    # Remove the salt value from the plaintext image data
+    plaintext = plaintext[16:]
+    # Shred the original encrypted data to prevent recovery
+    file1 = open(f, 'wb')
+    file1.write(os.urandom(os.path.getsize(f)))
+    file1.close()
     path=os.path.join(u,"decrypt")
     file1=open(os.path.join(path,"aesout.jpg"),"wb")
     file1.write(plaintext)
--- GUI.py
+++ GUI.py
@@ -1,10 +1,35 @@
+import cv2
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import simpledialog
 import os
 import AES_EncryptDecrypt
+import tkinter.colorchooser as cc
+from PIL import Image, ImageDraw, ImageTk
 
+class CustomButton(tk.Button):
+    def __init__(self, master=None, **kwargs):
+        super().__init__(master=master, **kwargs)
+        self.config(
+            bg="#2c3e50",  # background color
+            fg="#ecf0f1",  # foreground color
+            relief="flat",  # border style
+            bd=5,  # border width
+            highlightbackground="white",
+            padx=10,  # horizontal padding
+            pady=5,  # vertical padding
+            font=("Helvetica", 12, "bold")  # font style
+        )
+        self.config(bg=self["bg"], fg=self["fg"])
+        self.bind("<Enter>", self.on_enter)
+        self.bind("<Leave>", self.on_leave)
+    
+    def on_enter(self, event):
+        self.config(bg=self["fg"], fg=self["bg"])
+        
+    def on_leave(self, event):
+        self.config(bg=self["activebackground"], fg=self["activeforeground"])
 
 parent_dir=os.getcwd()+"/Database/"
 global enc_file
@@ -61,8 +86,6 @@ def Deckey():
     global dec_key
     if(dec_v.get()=="AES-512"):
         dec_key=simpledialog.askstring("password","Enter the password")
-
-   
     else:
         messagebox.showwarning("warning","No method selected")
 
@@ -73,8 +96,8 @@ def encrypt():
     if(enc_v.get()=="AES-512"):
         AES_EncryptDecrypt.aes_encrypt(parent_dir,enc_key,enc_file)
         tk.messagebox.showinfo("Success","Encryption Successful")
-    
     else:
+        messagebox.showwarning("warning","Encryption input not Found")
         print(enc_v.get())
 
 def decrypt():
@@ -85,34 +108,143 @@ def decrypt():
         AES_EncryptDecrypt.aes_decrypt(parent_dir,dec_key,dec_file)
         tk.messagebox.showinfo("success","Decryption Successful")
     else:
+        messagebox.showwarning("warning","Decryption input not Found")
         print(dec_v.get())
 
+def pick_color(labelframe1,labelframe2):
+    color = cc.askcolor()[1] # askcolor() returns a tuple of (color in RGB, color in hexadecimal), we only need the hexadecimal color
+    if color:
+        labelframe1.config(bg=color)
+        labelframe2.config(bg=color)
+
+# def about_frame():
+#     top = tk.Tk()
+
+def para_frame():
+    
+    # create new window
+    para_frame_1 = tk.Tk()
+    para_frame_1.title("About AES Encryption")
+
+    # set window size and position
+    para_frame_1.geometry("800x250+340+75")
+
+    text1 = tk.Text(para_frame_1, font=('Helvetica', 12))
+    text1.pack()
+    # create text widgets
+    text1.tag_configure( "bullet", font=('Helvetica', 12), foreground='black', background= '#ecf0f1', lmargin1=0, lmargin2=10, justify='left', tabs=('5'))
+    text1.insert(tk.END, '• The AES Encryption algorithm (also known as the Rijndael algorithm) is a symmetric block cipher algorithm with a block/chunk size of 128 bits. It converts these individual blocks using keys of 128, 192, and 256 bits. Once it encrypts these blocks, it joins them together to form the ciphertext\n', 'bullet')
+    text1.insert(tk.END, '• It is based on a substitution-permutation network, also known as an SP network. It consists of a series of linked operations, including replacing inputs with specific outputs (substitutions) and others involving bit shuffling (permutations)\n', 'bullet')
+    text1.insert(tk.END, '• For encryption, each round consists of the following four steps:\n1) Substitute bytes\n2) Shift rows\n3) Mix columns\n4) Add round key\n• The last step consists of XORing the output of the previous three steps with four words from the key schedule.\n\n\n', 'bullet')
+    text1.pack(fill="both", expand=True)
+
+
+
+    para_frame_2 = tk.Tk()
+    para_frame_2.title("About AES Decryption")
+
+    # set window size and position
+    para_frame_2.geometry("800x230+340+450")
+
+    text2 = tk.Text(para_frame_2, font=('Helvetica', 12))
+    text2.pack(fill="both", expand=True)
+    # create text widgets
+    text2.tag_configure( "bullet", font=('Helvetica', 12), foreground='black', background= '#ecf0f1', lmargin1=0, lmargin2=10, justify='left', tabs=('5'))
+    text2.insert(tk.END, '• AES is a symmetric algorithm which uses the same 128, 192, or 256 bit key for both encryption and decryption (the security of an AES system increases exponentially with key length)\n', 'bullet')
+    text2.insert(tk.END, '• For decryption, each round consists of the following four steps: \n1) Inverse shift rows\n2) Inverse substitute bytes\n3) Add round key\n4) Inverse mix columns\n• The third step consists of XORing the output of the previous two steps with four words from the key schedule\n', 'bullet')
+    text2.insert(tk.END, '• The last round for encryption does not involve the “Mix columns” step. The last round for decryption does not involve the “Inverse mix columns” step.\n\n\n\n\n', 'bullet')
+
+    text2.pack()
+
+def db_connect():
+    messagebox.showinfo("Info", "This feature is launching soon!")
+
+def capture_image():
+    messagebox.showinfo("Alert", "Capturing Image!")
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    cap.release()
+    cv2.imwrite("captured_image.jpg", frame)
+    image = Image.open("captured_image.jpg")
+    photo = ImageTk.PhotoImage(image)
+
+    messagebox.showinfo("Info", "Image captured and saved successfully!")
 
 def frame():
+    # Adjusting the Window size of the GUI form the top left corner
     top = tk.Tk()
-    top.geometry("490x400")
+    screen_width = top.winfo_screenwidth()
+    screen_height = top.winfo_screenheight()
+    top.geometry("%dx%d+0+0" % (screen_width, screen_height))
     top.configure(bg="black")
     top.title("Image Cryptography")
-    labelframe2=tk.LabelFrame(top,text="Image Encryption: ",height=175,width=470,bg="light blue").place(x=10,y=10)
-    labelframe3=tk.LabelFrame(top,text="Image Decryption: ",height=185,width=470,bg="light blue").place(x=10,y=205)
-    
+    labelframe2=tk.LabelFrame(top,text="IMAGE ENCRYPTION ",height=355,bg="#ECF9FF",width=1500,font=("Helvetica", 24))
+    labelframe2.place(x=150,y=0)
+    labelframe3=tk.LabelFrame(top,text="IMAGE DECRYPTION ",height=400,bg="#ECF9FF",width=1500,font=("Helvetica", 24))
+    labelframe3.place(x=150,y=356)
+
+    menu_frame = tk.Frame(top, bg="#FFE7CC", width=200)
+
+    # create the menu items as buttons
+    home_btn = tk.Button(menu_frame, text="Home", bg="#FFFBEB", fg="black", activebackground="white", width=20)
+    about_btn = tk.Button(menu_frame, text="About", bg="#FFFBEB", fg="black", width=20, command=para_frame)
+    contact_btn = tk.Button(menu_frame, text="Connect DB", bg="#FFFBEB", fg="black", width=20, command=db_connect)
+    featuresButton = tk.Label(menu_frame, text="Features", bg="#FFFBEB", fg="black",width=22).place(y=500)
+    capture_button = tk.Button(menu_frame, text="Capture Image", bg="#FFFBEB", fg="black", width=20, command=capture_image)
+    # pack the menu items into the menu frame
+    home_btn.pack(pady=10)
+    about_btn.pack(pady=10)
+    contact_btn.pack(pady=10)
+
+    # pack the menu frame into the main window
+    menu_frame.pack(side="left", fill="y")
+
+    # create a new image with size 16x16 and color mode "RGB"
+    image = Image.new("RGB", (16, 16), (255, 255, 255))
+
+    # create a draw object
+    draw = ImageDraw.Draw(image)
+
+    # draw a circle in the center of the image with radius 7 and outline color (0, 0, 0)
+    draw.ellipse((4, 4, 11, 11), outline=(0, 0, 0))
+
+    # draw a smaller circle inside the first circle with radius 4 and fill color (255, 0, 0)
+    draw.ellipse((6, 6, 9, 9), fill=(255, 0, 0))
+
+    # save the image to a file
+    image.save("colorpicker.png")
+
+    icon = Image.open("colorpicker.png")
+
+    # create a PhotoImage object from the icon
+    photo = ImageTk.PhotoImage(icon)
+
+    # create the buttons to pick the colors
+    button2 = tk.Button(top, image=photo, command=lambda: pick_color(labelframe2,labelframe3),fg="black").place(x=10, y=540)
+    # Create a label for the text
+    text_label = tk.Label(top,text= "change Bg color",bg="#FFE7CC")
+    text_label.pack(side="left")
+    text_label.place(x=40, y=540)
+
+    capture_button.place(x=0, y=590)
+
     global i
     global enc_v
     global enc_key
     global enc_file
     listus=["AES-512"]
     enc_v=tk.StringVar()
-    enc_v.set("None")
+    enc_v.set("Select")
     i=tk.StringVar()
     
-    inputEncFile = tk.Label(labelframe2, text="1. Select the Image:").place(x=10,y=50)
-    inputEncFileEntry = tk.Entry(labelframe2,width=42,textvariable=i).place(x=135,y=50)
-    inputEncBtn = tk.Button(labelframe2, text="Browse ...",width=10, height=1,bg="green yellow",command=openfileEnc).place(x=395,y=46)
-    recEncFile = tk.Label(labelframe2, text="2. Select the Method:").place(x=10,y=90)
-    reclistBtn=tk.OptionMenu(labelframe2,enc_v,*listus).place(x=135,y=87,width=100)
-    keyEncFile = tk.Label(labelframe2, text="3. Generate Key:").place(x=10,y=130)
-    keyBtn=tk.Button(labelframe2,text="Input Key", height=1,activebackground="cyan",command=key).place(x=135,y=130,width=100)
-    EncryptBTN=tk.Button(labelframe2,text="Encrypt",width=20, height=3,activebackground="green",command=encrypt).place(anchor="e",relx=.85,rely=.35)
+    inputEncFile = tk.Label(labelframe2, text="Select Image",font=("Arial", 14)).place(x=200,y=50)
+    inputEncFileEntry = tk.Entry(labelframe2,width=42,textvariable=i,font=("Arial", 14)).place(x=350,y=50)
+    inputEncBtn = tk.Button(labelframe2, text="Browse",width=10, height=1,bg="blue",command=openfileEnc,font=("Arial", 14)).place(x=850,y=46)
+    recEncFile = tk.Label(labelframe2, text="Select Method",font=("Arial", 14)).place(x=200,y=100)
+    reclistBtn=tk.OptionMenu(labelframe2,enc_v,*listus).place(x=350,y=97,width=100)
+    keyEncFile = tk.Label(labelframe2, text="Generate Key",font=("Arial", 14)).place(x=200,y=150)
+    keyBtn=tk.Button(labelframe2,text="Input Key", height=1,activebackground="white",command=key,font=("Arial", 14)).place(x=350,y=150,width=100)
+    EncryptBTN= CustomButton(labelframe2,text="Encrypt",width=20, height=2,activebackground="#FFE7CC",command=encrypt,font=("Arial", 14),borderwidth=5).place(anchor="e",relx=.55,rely=.65)
 
     global j
     global dec_key
@@ -120,17 +252,17 @@ def frame():
     global dec_v
     j=tk.StringVar()
     dec_v=tk.StringVar()
-    dec_v.set("None")
+    dec_v.set("Select")
 
 
-    inputDecFile = tk.Label(labelframe2, text="1. Select the Image:").place(x=10,y=245)
-    inputDecFileEntry = tk.Entry(labelframe2,width=42,textvariable=j).place(x=135,y=245)
-    inputDecBtn = tk.Button(labelframe2, text="Browse ...",width=10, height=1,bg="green yellow",command=openfileDec).place(x=395,y=241)
-    recDecFile = tk.Label(labelframe2, text="2. Select the Method:").place(x=10,y=285)
-    reclistBtn=tk.OptionMenu(labelframe2,dec_v,*listus).place(x=135,y=282,width=100)
-    keyEncFile = tk.Label(labelframe2, text="3. Select secret:").place(x=10,y=325)
-    keyBtn=tk.Button(labelframe2,text="Input Key", height=1,activebackground="cyan",command=Deckey).place(x=135,y=325,width=100)
-    EncryptBTN=tk.Button(labelframe2,text="Decrypt",width=20, height=3,activebackground="red",command=decrypt).place(anchor="e",relx=.85,rely=.85)
+    inputDecFile = tk.Label(labelframe3, text="Select Image",font=("Arial", 14)).place(x=200,y=50)
+    inputDecFileEntry = tk.Entry(labelframe3,width=42,textvariable=j,font=("Arial", 14)).place(x=350,y=50)
+    inputDecBtn = tk.Button(labelframe3, text="Browse",width=10, height=1,bg="blue",command=openfileDec,font=("Arial", 14)).place(x=850,y=46)
+    recDecFile = tk.Label(labelframe3, text="Select Method",font=("Arial", 14)).place(x=200,y=100)
+    reclistBtn=tk.OptionMenu(labelframe3,dec_v,*listus).place(x=350,y=97,width=100)
+    keyEncFile = tk.Label(labelframe3, text="Select secret",font=("Arial", 14)).place(x=200,y=150)
+    keyBtn=tk.Button(labelframe3,text="Input Key", height=1,activebackground="white",command=Deckey,font=("Arial", 14)).place(x=350,y=150,width=100)
+    EncryptBTN=CustomButton(labelframe3,text="Decrypt",width=20, height=2,activebackground="#FFE7CC",command=decrypt,font=("Arial", 14)).place(anchor="e",relx=.55,rely=.65)
 
     top.mainloop()
 
